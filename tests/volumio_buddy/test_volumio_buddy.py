1a0d7fad25241f7d512d1c1a2032150856430874
Michiel Fokke <github@fokke.org>
2017-01-16 23:23:59 +0100

    Added first unit tests

--- tests/volumio_buddy/test_volumio_buddy.py
+++ tests/volumio_buddy/test_volumio_buddy.py
@@ -4,11 +4,30 @@
 
 import unittest
 import mock
+import wiringpi
+from mock import patch
 
-from volumio_buddy import volumio_buddy_setup
+from volumio_buddy import volumio_buddy_setup, PushButton
 
-@patch('wiringpi.wiringPiSetup')
-def test_volumio_buddy_setup(mock_setup):
-    volumio_buddy_setup()
-    volumio_buddy_setup()
-    mock_setup.assert_called_once_with()
+@patch('wiringpi.wiringPiSetup', autospec=True)
+@patch('wiringpi.pinMode', autospec=True)
+@patch('wiringpi.wiringPiISR', autospec=True)
+
+class TestVolumioBuddy(unittest.TestCase):
+        
+    def test0_volumio_buddy_setup(self, mock_wiringpi_ISR, mock_wiringpi_pinMode, mock_wiringpi_setup):
+        volumio_buddy_setup()
+        volumio_buddy_setup()
+        mock_wiringpi_setup.assert_called_once_with()
+
+    def test1_PushButton(self, mock_wiringpi_ISR, mock_wiringpi_pinMode, mock_wiringpi_setup):
+        def callback_function():
+            pass
+        gpio_pin = 1
+        p = PushButton(gpio_pin)
+        mock_wiringpi_pinMode.assert_called_with(gpio_pin, 0)
+        p.set_callback(callback_function)
+        mock_wiringpi_ISR.assert_called_with(gpio_pin, wiringpi.INT_EDGE_BOTH, p._callback)
+
+if __name__ == '__main__':
+    unittest.main()
